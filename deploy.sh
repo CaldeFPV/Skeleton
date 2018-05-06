91e153ebff0ded5b89070e3d40b28ade7bbf7590
Scott M <mccaffrey.scott@gmail.com>
2018-05-05 18:52:02 -0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+rm -rf /var/www/html/*
 cp -R ./* /var/www/html/
 rm /var/www/html/deploy.sh
+
+#cp -R ./* /mnt/c/Users/smccaffr/Documents/MySite3/
+
 
 #another flag would be rm -rf whole site
 #since the above copy will leave behind 
 #anything not overwritten
 
 #this doesn't seem necessary but could be a good flag option
-#service apache2 restart
+service apache2 restart
